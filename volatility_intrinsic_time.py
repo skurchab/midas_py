f033b397884b7acdc4b5028caa9165f3a1e8f6f5
skurchab <solomiia.kurchaba@smcebi.edu.pl>
2020-01-08 20:56:33 +0100

    Intrinsic time index added

--- volatility_intrinsic_time.py
+++ volatility_intrinsic_time.py
@@ -69,42 +69,60 @@ df = df.rename(columns={"DateID": 'Date',
                    'Number of Transactions': 'Transaction_nb',
                    'Closing Price': 'Price'})
 df = df.dropna(subset = ['Price']).reset_index(drop=True)
-df['Int_event'] = intrinsic_event(df['Price'], d_up=3, d_down=3, mode='up')
-
-print(df.Int_event.unique())
-
+df['Int_event'] = intrinsic_event(df['Price'], d_up=40, d_down=40, mode='up')
+# Set a new Intrinsic time index
+cur_index = 0
+df['Int_index'] = None
+for i in range(len(df)):
+    if df['Int_event'][i] in [-1, 1, -2, 2]:
+        cur_index = cur_index + 1
+        df['Int_index'].iloc[i] = cur_index
+# Transformation of time index
+df['Date'] = pd.to_datetime(df['Date'], format='%Y%m%d', errors='ignore')
+print(df.iloc[:43])
 # Data visualisation
-
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].hist(df.Variance, bins=50, log=True)
-axs[0, 0].set_title('Variance')
-axs[0, 1].hist(df.Return, bins=50, log=True)
-axs[0, 1].set_title('Return')
-axs[1, 0].hist(df.Price, bins=50, log=True)
-axs[1, 0].set_title('Closing Price')
-axs[1, 1].hist(df.Transaction_nb, bins=50, log=True)
-axs[1, 1].set_title('Number of Transactions')
+plt.plot(df.index[:300], df.Price[:300])
+plt.plot(df[(df.Int_event==1) | (df.Int_event==-1)].loc[:300].index,
+         df[(df.Int_event==1) | (df.Int_event==-1)].loc[:300].Price, 'o')
+plt.plot(df[(df.Int_event==2) | (df.Int_event==-2)].loc[:300].index,
+         df[(df.Int_event==2) | (df.Int_event==-2)].loc[:300].Price, marker='v', linestyle='--')
 plt.show()
 
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].plot(df.Variance)
-axs[0, 0].set_title('Variance')
-axs[0, 1].plot( df.Return)
-axs[0, 1].set_title('Return')
-axs[1, 0].plot(df.Price)
-axs[1, 0].plot(df[(df.Int_event==2) | (df.Int_event==-2)].Price, 'o', ms=0.3)
-axs[1, 0].set_title('Closing Price')
-axs[1, 1].plot(df.Transaction_nb)
-axs[1, 1].set_title('Number of Transactions')
-plt.show()
 
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].plot(df[(df.Int_event==2) | (df.Int_event==-2)].Variance)
-axs[0, 0].set_title('Variance')
-axs[0, 1].plot( df[(df.Int_event==2) | (df.Int_event==-2)].Return)
-axs[0, 1].set_title('Return')
-axs[1, 0].plot(df[(df.Int_event==2) | (df.Int_event==-2)].Price)
-axs[1, 0].set_title('Closing Price')
-axs[1, 1].plot(df[(df.Int_event==2) | (df.Int_event==-2)].Transaction_nb)
-axs[1, 1].set_title('Number of Transactions')
-plt.show()
+#fig, axs = plt.subplots(2, 2)
+#axs[0, 0].hist(df.Variance, bins=50, log=True)
+#axs[0, 0].hist(df[(df.Int_event==1) | (df.Int_event==-1)].Variance, bins=50, log=True)
+#axs[0, 0].set_title('Variance')
+#axs[0, 1].hist(df.Return, bins=50, log=True)
+#axs[0, 1].hist(df[(df.Int_event==1) | (df.Int_event==-1)].Return, bins=50, log=True)
+#axs[0, 1].set_title('Return')
+#axs[1, 0].hist(df.Price, bins=50, log=True)
+#axs[1, 0].hist(df[(df.Int_event==1) | (df.Int_event==-1)].Price, bins=50, log=True)
+#axs[1, 0].set_title('Closing Price')
+#axs[1, 1].hist(df.Transaction_nb, bins=50, log=True)
+#axs[1, 1].hist(df[(df.Int_event==1) | (df.Int_event==-1)].Transaction_nb, bins=50, log=True)
+#axs[1, 1].set_title('Number of Transactions')
+#plt.show()
+
+#fig, axs = plt.subplots(2, 2)
+#axs[0, 0].plot(df.Variance)
+#axs[0, 0].set_title('Variance')
+#axs[0, 1].plot( df.Return)
+#axs[0, 1].set_title('Return')
+#axs[1, 0].plot(df.Price)
+#axs[1, 0].plot(df[(df.Int_event==1) | (df.Int_event==-1)].Price, 'o', ms=0.5)
+#axs[1, 0].set_title('Closing Price')
+#axs[1, 1].plot(df.Transaction_nb)
+#axs[1, 1].set_title('Number of Transactions')
+#plt.show()
+
+#fig, axs = plt.subplots(2, 2)
+#axs[0, 0].plot(df[(df.Int_event==1) | (df.Int_event==-1)].Variance)
+#axs[0, 0].set_title('Variance')
+#axs[0, 1].plot( df[(df.Int_event==1) | (df.Int_event==-1)].Return)
+#axs[0, 1].set_title('Return')
+#axs[1, 0].plot(df[(df.Int_event==1) | (df.Int_event==-1)].Price)
+#axs[1, 0].set_title('Closing Price')
+#axs[1, 1].plot(df[(df.Int_event==1) | (df.Int_event==-1)].Transaction_nb)
+#axs[1, 1].set_title('Number of Transactions')
+#plt.show()
